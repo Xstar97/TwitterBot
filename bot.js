1d65f8fa39b4844de0b7d735eb6c76af6e3686f7
Xstar97 <Xstar97@users.noreply.github.com>
2021-05-13 03:47:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,63 +1,62 @@
-const { auth, sleep, postReplyWithMedia, postReply  } = require('./config.js');
+const { auth, configData, fs, sleep, postReplyWithMedia, postReply  } = require('./config.js');
 const client = auth();
 
 //variables
-var count = 0;
-var countMax = 10;//sleeps once max count is hit.
-var sleepTimer=3;//mins
-var tag = "random word filter";//#noob
-var botName = "userName";
+var count = 0;//inital count
+var params = configData.parameters;//param filter for stream
+var botName = configData.botName;//user name of author or bot
+var tag = params.track;//filtered text
+var countMax = configData.countMax;//sleeps once max count is hit.
+var sleepTimer = configData.sleepTimer;//X mins for sleep function
+var mediaDir = configData.mediaDir;//media location
+var msg = configData.msgs;//messages for victims
 
-const parameters = {
-  track: tag,
-  lang: 'en',
-  locale: 'en'
-};
-
-let msg = 
-[
-'Random message 1',
-'Random message 2',
-'Random message 3'
-]
-
+//polls the list for a random msg
 function getMsg(){
-	var mth = Math.floor(Math.random() * msg.length);
-return msg[mth];	
+	var mth1 = Math.floor(Math.random() * msg.length);
+return msg[mth1];	
 }
 
+//polls the list for a random media
+function getMedia(){
+var media = fs.readdirSync(mediaDir);
+var mth4 = Math.floor(Math.random() * media.length);
+return mediaDir+media[mth4];	
+}
+
+//increments counter
 function counterInc(){
 	count+=1;
 	console.log("message count: " + count + "\n");
 }
 
+//pauses thread after count spills over.
 function sleeper(){
 	try{
-	if(count > countMax){
-		console.log("meesage count max of " + countMax + ": " + count);
-		console.log("shutting down for " + sleepTimer + " min(s)");
-		//sleeps for x mins
-		sleep.sleep(sleepTimer * 60);	
-		count = 0;
-		console.log("\nstarting up again!\n");
-	}
+		if(count > countMax){
+			console.log("meesage count max of " + countMax + ": " + count + "\n");
+			console.log("shutting down for " + sleepTimer + " min(s)");
+			//sleeps for x mins
+			sleep.sleep(sleepTimer * 60);	
+			count = 0;
+			console.log("\nstarting up again boy!\n");
+		}
 	}catch(e){
 		console.log("e: " + e);
 	}
 }
 
-
 //Seaching a filtered stream of tweets
-client.stream('statuses/filter', parameters, function (stream) {
+client.stream('statuses/filter', params, function (stream) {
 	console.log("Searching for " + tag + " tweets...");
 	// when a tweet is found
 	stream.on('data', function (tweet) {
-		
 		try
 		{
 			//checking if tweet auther is not the bot or author
 			var userName = tweet.user.screen_name != botName;
-			var tweetData = tweet.text.includes(tag) && tweet.text != null;//tweet.text.length != 0 && tweet.text != null
+			var tweetData = tweet.text.includes(tag) && tweet.text != null;
+			//tweet.text.length != 0 && tweet.text != null
 			
 			if(userName && tweetData)
 			{
@@ -69,27 +68,22 @@ client.stream('statuses/filter', parameters, function (stream) {
 					console.log("User: " + tweet.user.screen_name + " tweeted: \n", tweet.text);
 					
 					//our message
-			        var message = getMsg();
-			
-			       //TODO uncomment the tweet option
-			       //tweet the victim
-			       //postReply(client, message, tweet);
-			       //console.log("\nreplying with a txt\n");
-			
-			       //postReplyWithImg(client, "./img.png", message, tweet);
-			       //console.log("\nreplying with a image and txt\n");
-				
-		               //postReplyWithMedia(client, "./animGif.gif", message, tweet);
-			       //console.log("\nreplying with a gif and txt\n");
-			
-		               //postReplyWithMedia(client, "./video.mp4", message, tweet);
-			       //console.log("\nreplying with a vid and txt\n");
+					var message = getMsg();
+					var media = getMedia();
 					
-			      //incrementing counter
-				counterInc();
+					////TODO uncomment the tweet option
+			               //tweet the victim
+			               //postReply(client, message, tweet);
+			               //console.log("\nreplying with a txt\n");
+			
+			               //postReplyWithMedia(client, media, message, tweet);
+			               //console.log("\nreplying with ANY media and txt\n");
 					
-				//sleeper function
-				sleeper();
+					//incrementing counter
+					counterInc();
+					
+					//sleeper function
+					sleeper();
 				}
 			}
 			//end of the line
