ab5086e948554d4bf5ba61c6ef8714c75f2ba464
Xstar97 <Xstar97@users.noreply.github.com>
2021-05-18 03:15:22 -0400

    Update bot.js

--- v1.1/bot.js
+++ v1.1/bot.js
@@ -1,30 +1,32 @@
 const { auth, configData, BotTimer, fs, postReplyWithMedia, postReply, postTweet } = require('./config.js');
+const Path = require("path");
 const client = auth();
 process.stdin.resume();//so the program will not close instantly
+const exit_time_seconds = 5;
 
 function exitHandler(options, exitCode) {
-    if (options.cleanup) console.log("cleanup");
-    if (exitCode || exitCode === 0) console.log(exitCode);
-    if (options.exit) {
+	if (options.cleanup) console.log("cleanup");
+	if (exitCode || exitCode === 0) console.log(exitCode);
+	if (options.exit) {
 		endMsgBot();
-		setTimeout(function() {
+		setTimeout(function () {
 			process.exit()
-		}, 5000);
+		}, exit_time_seconds * 1000);
 	};
 }
 
 //do something when app is closing
-process.on('exit', exitHandler.bind(null,{cleanup:true}));
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
 
 //catches ctrl+c event
-process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
 
 // catches "kill pid" (for example: nodemon restart)
-process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
-process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
 
 //catches uncaught exceptions
-process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
 
 //variables
 var params = configData.parameters;//param filter for stream
@@ -38,6 +40,27 @@ var _botMsg = configData.botMsgList;
 var delaybotAction = configData.botActionDelayInSeconds;
 var delayMsgbotAction = configData.botMsgActionDelayInMinutes;
 
+var mediaShuffle = configData.canShuffleMediaList;
+var msgShuffle = configData.canShuffleMsgList;
+
+function shuffle(array) {
+	var currentIndex = array.length, temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
 
 function randomNumber(list) {
 	return Math.floor(Math.random() * list);
@@ -51,6 +74,16 @@ function getListDataByLang(list, lang) {
 	return iList;
 }
 
+function ThroughDirectory(Directory) {
+	let Files = [];
+	fs.readdirSync(Directory).forEach(File => {
+		const Absolute = Path.join(Directory, File);
+		if (fs.statSync(Absolute).isDirectory()) return ThroughDirectory(Absolute);
+		else return Files.push(Absolute);
+	});
+	return Files;
+}
+
 //polls the list for a random msg
 function getMsg(lang) {
 	var mth1 = randomNumber(_msg.length);
@@ -58,22 +91,33 @@ function getMsg(lang) {
 	if (typeof msg === 'undefined') {
 		msg = _msg[mth1][defLang];
 	}
+	if (msgShuffle) {
+		msg = shuffle(msg);
+		console.log("\nmsg list shuffled!\n");
+	}
 	return msg;
 }
 
 //polls the list for a random media
 function getMedia(lang) {
-	var media = fs.readdirSync(getListDataByLang(_mediaDir, lang));
-	var dir = getListDataByLang(_mediaDir, lang);
+	var media = ThroughDirectory(getListDataByLang(_mediaDir, lang));
+	if (mediaShuffle) {
+		media = shuffle(media);
+		console.log("\nmedia list shuffled!\n");
+	}
+
 	var mth2 = randomNumber(media.length);
-	return dir + media[mth2];
+	var randomMedia = media[mth2];
+	return randomMedia;
 }
 
 function getBotMsg(action) {
-	return getListDataByLang(action, defLang).replace('{TAG}', tag) + "\nbot says...\n" + getMsg(defLang);
+	var msg = `${getListDataByLang(action, defLang).replace('{TAG}', tag)}\nbot says...\n${getMsg(defLang)}`;
+	return msg;
 }
+//fire off a reply with media
 const botAction = async (client, tweet) => {
-	console.log("\nreplying with a funny tweet in " + delaybotAction.tweet + "s\n");
+	console.log(`\nreplying with a funny tweet in ${delaybotAction.tweet}s\n`);
 	await new Promise(resolve => {
 		setTimeout(() => {
 			//our message
@@ -82,18 +126,26 @@ const botAction = async (client, tweet) => {
 
 			//tweeting victim
 			postReplyWithMedia(client, media, message, tweet);
-			console.log("\nreplying with random media and msg\n");
+			console.log(`\nreplying with a random:\nmedia: ${media}\nmsg ${message}`);
 		}, delaybotAction.tweet * 1000)
 	})
 }
 
+function startMsgBot() {
+	var startMsg = getBotMsg(_botMsg.start);
+	postTweet(client, startMsg);
+}
+
 var minutesStale = delayMsgbotAction.stale, the_intervalStale = minutesStale * 60 * 1000;
 var staleCounter = 0;
+/*
+stale message timer
+*/
 function staleMsgBot() {
-	console.log("stale bot:" + minutesStale + " minute check");
+	console.log(`stale bot: ${minutesStale} minute check`);
 	getMsg(defLang);
 	var tweet = getBotMsg(_botMsg.stale);
-	console.log("tweet: " + tweet);
+	console.log(`tweet: ${tweet}`);
 	postTweet(client, tweet);
 	staleCounter += 1;
 	if (staleCounter == 5) {
@@ -102,15 +154,10 @@ function staleMsgBot() {
 }
 var staleTimer = new BotTimer(function () { staleMsgBot() }, the_intervalStale);
 
-function startMsgBot() {
-	var startMsg = getBotMsg(_botMsg.start);
-	postTweet(client, startMsg);
-}
-
 const endMsgBot = async () => {
 	console.log("end of life");
 	var tweet = getBotMsg(_botMsg.end);
-	console.log("tweet: " + tweet);
+	console.log(`tweet: ${tweet}`);
 	postTweet(client, tweet);
 }
 
@@ -123,24 +170,26 @@ function startBot() {
 
 	//Seaching a filtered stream of tweets
 	client.stream('statuses/filter', params, function (stream) {
-		console.log("Searching for " + tag + " tweets...");
+		console.log(`Searching for ${tag} tweets...`);
 
 		// when a tweet is found
 		stream.on('data', function (tweet) {
+			var user = tweet.user.screen_name;
+			var status = tweet.text;
 			//checking if tweet auther is not the bot or author
-			var ignoreUser = ignoreList.includes(tweet.user.screen_name);
-			var userName = tweet.user.screen_name != botName && !ignoreUser;
+			var ignoreUser = ignoreList.includes(user);
+			var userNames = user != botName && !ignoreUser;
 
 			if (ignoreUser) {
-				console.log("\nuser: " + tweet.user.screen_name + " is on the naughty list!\n");
+				console.log(`\nuser: ${user} is on the naughty list!\n`);
 			}
 
-			var tweetData = tweet.text != null && tweet.text.includes(tag);
-			if (userName && tweetData) {
+			var tweetData = status != null && status.includes(tag) && tweet.retweeted_status == null;
+			if (userNames && tweetData) {
 				//logging a tweet was found
 				console.log("\nFound a tweet!\n__________________________\n");
 				//user name and tweet
-				console.log("User: " + tweet.user.screen_name + " tweeted: \n", tweet.text);
+				console.log(`User: ${user} tweeted: ${status}`);
 				//botAction
 				botAction(client, tweet);
 				//reset stale timer
